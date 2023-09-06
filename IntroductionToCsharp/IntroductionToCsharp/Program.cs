14808197ced1f17a028f1ea0eb8ce0727dfaa725
Sandip Patil <sandip50sandip@gmail.com>
2023-09-06 16:41:49 +0530

    IntroductionToCsharp

---
+++ IntroductionToCsharp/IntroductionToCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IntroductionToCsharp
+{
+    internal class Program
+    {
+        static void Main1()
+        {
+            //Console.Write("Hello, Csharp!"); single line comment
+
+            /*
+            multi line comment
+            Console.WriteLine("Hello, World!");
+            */
+
+            Console.WriteLine("Hello, Main1");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Hello, Main");
+            Main1();
+        }
+    }
+}
