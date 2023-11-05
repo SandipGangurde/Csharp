82fe8d727799c11eb0b92ea57beb14ab18476383
Sandip Patil <sandip50sandip@gmail.com>
2023-11-05 16:03:44 +0530

    Added Category controller model and dto

---
+++ LearnAPI/MagicVilla/MagicVilla_VillaAPI/Controllers/ProductCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<CategoryDto> GetCategories()
+        {
+            return new List<CategoryDto>()
+            {
+                new CategoryDto { Id = 1, Name = "TV"  },
+                new CategoryDto { Id = 2, Name = "Laptop" },
+                new CategoryDto { Id = 3, Name = "Mobile"}
+            };
+        }
+    }
+}
